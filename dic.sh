90af3ea7530a4de88803ff9c0056b3008d155beb
shang.chen <shang.chen@fenbeitong.com>
2017-01-07 21:26:22 +0800

    init

---
+++ dic.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+if [ $# = 1 ] && [ $1 = '?' ];then
+  echo "sh dic.sh {需要翻译的单词或句子}" 
+  exit
+fi
+p=$*
+echo '>>>>>>>>>' ${p} '<<<<<<<<<'
+# 转换成 url 编码，主要针对汉字进行
+# query=`echo $p | tr -d '\n' | xxd -plain | sed 's/\(..\)/%\1/g'`
+
+result=`curl http://fanyi.youdao.com/openapi.do?keyfrom=macSay\&key=1081776100\&type=data\&doctype=json\&version=1.1 --data-urlencode q="$p"`
+#echo $result
+
+# 记录查询历史
+now=`date "+%Y-%m-%d %H:%M:%S"`
+`echo $now"===\n$p===>"$result >> ~/.dichistory`
+
+code=`echo $result | jq '.errorCode'`
+if [ "$code" = "20" ];then 
+  echo '要翻译的文本过长'
+  exit
+elif [ "$code" = "30" ];then
+  echo '无法进行有效的翻译'
+  exit
+elif [ "$code" = "40" ];then
+  echo '不支持的语言类型'
+  exit
+elif [ "$code" = "50" ];then
+  echo '无效的key'
+  exit
+elif [ "$code" = "60" ];then
+  echo '无词典结果，仅在获取词典结果生效'
+  exit
+else
+    explains=`echo $result | jq '.basic|.explains'`
+    if [ "$explains" != "null" ];then
+       `say $p`
+       echo $explains | jq .
+    else
+      echo $result | jq '.translation'
+    fi
+fi
