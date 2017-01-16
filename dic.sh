58847172c5cd698fe96086f77edc10c31a9a71f4
shang.chen <shang.chen@fenbeitong.com>
2017-01-17 00:19:16 +0800

    只有返回正确结果的时候才记录

--- dic.sh
+++ dic.sh
@@ -12,32 +12,23 @@ echo '>>>>>>>>>' ${p} '<<<<<<<<<'
 
 history=`less ~/.dichistory |grep -iw "$p===>"| awk -F "===>" '{print $2}'`
 
-if [ x"$history" = 'x' ];then
-  result=`curl http://fanyi.youdao.com/openapi.do?keyfrom=macSay\&key=1081776100\&type=data\&doctype=json\&version=1.1 --data-urlencode q="$p"`
-  # 记录查询历史
-  now=`date "+%Y-%m-%d %H:%M:%S"`
-  `echo $now"===\n$p===>"$result >> ~/.dichistory`
-else
-  result=$history 
-fi
-
-code=`echo $result | jq '.errorCode'`
-if [ "$code" = "20" ];then 
-  echo '要翻译的文本过长'
-  exit
-elif [ "$code" = "30" ];then
-  echo '无法进行有效的翻译'
-  exit
-elif [ "$code" = "40" ];then
-  echo '不支持的语言类型'
-  exit
-elif [ "$code" = "50" ];then
-  echo '无效的key'
-  exit
-elif [ "$code" = "60" ];then
-  echo '无词典结果，仅在获取词典结果生效'
-  exit
-else
+function search(){
+  if [ "$code" = "20" ];then 
+    echo '要翻译的文本过长'
+    exit
+  elif [ "$code" = "30" ];then
+    echo '无法进行有效的翻译'
+    exit
+  elif [ "$code" = "40" ];then
+    echo '不支持的语言类型'
+    exit
+  elif [ "$code" = "50" ];then
+    echo '无效的key'
+    exit
+  elif [ "$code" = "60" ];then
+    echo '无词典结果，仅在获取词典结果生效'
+    exit
+  else
     explains=`echo $result | jq '.basic|.explains'`
     if [ "$explains" != "null" ];then
        `say $p`
@@ -45,4 +36,22 @@ else
     else
       echo $result | jq '.translation'
     fi
+  fi
+}
+
+
+if [ x"$history" = 'x' ];then
+  result=`curl http://fanyi.youdao.com/openapi.do?keyfrom=macSay\&key=1081776100\&type=data\&doctype=json\&version=1.1 --data-urlencode q="$p"`
+  code=`echo $result | jq '.errorCode'`
+  search
+  if [ "$code" = "0" ];then
+    # 记录查询历史
+    now=`date "+%Y-%m-%d %H:%M:%S"`
+    `echo $now"===\n$p===>"$result >> ~/.dichistory`
+  fi
+else
+  result=$history 
+  code=`echo $result | jq '.errorCode'`
+  search
 fi
+
